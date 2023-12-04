7d33443affe8857b0cf7e5c47a0b103e5520ce1c
Rogelio <rogelio.cardenas02@utrgv.edu>
2023-12-04 17:24:27 -0600

    Added HTML file

--- app.py
+++ app.py
@@ -1,2 +1,19 @@
 #First Commit
 print("Hello World")
+#Importing libraries and packages useful functions
+from flask import Flask, render_template
+#Creating web app using Flask
+app = Flask(__name__)
+#creating route to the html file
+@app.route('/')
+def home():
+    return render_template("index.html")
+#runs the web app
+app.run(
+    debug = True
+)
+#to host online --> render.com
+#--> Get Started sign in with GitHub --> New+
+#--> web service --> build&deploy from Git repo
+#connect 1101-Final --> add name and add file in folder
+# requirements.txt
